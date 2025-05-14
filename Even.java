5e4f27b61b3cec3b20b54dbdd363ac8bb0715a44
juktimath003 <juktiamthshreya123@gmail.com>
2025-05-14 12:16:06 +0530

    save

---
+++ Even.java
@@ -0,0 +1,11 @@
+package cie;
+public class Even {
+    public static void main(String[] args) {
+        System.out.println("Even Numbers from 1 to 50:");
+        for(int i=2;i<=50;i++){
+            if(i%2==0){
+                System.out.println(" "+i);
+            }
+        }
+    }
+}
